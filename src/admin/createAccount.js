5d7076b601c2f494fbbc0bc9c7b3b5f56a66424f
PureGero <puregero@gmail.com>
2022-01-25 10:29:28 +1000

    Add billing details to createAccount form

--- src/admin/createAccount.js
+++ src/admin/createAccount.js
@@ -8,13 +8,28 @@ export function renderCreateAccount() {
   document.querySelector('head').appendChild(script);
 
   document.querySelector('main').innerHTML = `
-    <form>
+    <form class="createaccount">
       <h2 id="name">Create an account</h2>
       <p class="error" aria-live="polite"></p>
+      <label for="username">Name:</label>
+      <input type="text" name="name" id="name" placeholder="Name" autofocus/>
+      <label for="username">Company name:</label>
+      <input type="text" name="company" id="company" placeholder="Company name"/>
       <label for="username">Email:</label>
-      <input type="email" name="email" id="email" placeholder="Email" autofocus/>
+      <input type="email" name="email" id="email" placeholder="Email"/>
       <label for="password">Password:</label>
       <input type="password" name="password" id="password" placeholder="Password"/>
+      <label for="billingaddress1">Billing address:</label>
+      <input type="text" name="billingaddress1" id="billingaddress1" placeholder="Line 1"/>
+      <input type="text" name="billingaddress2" id="billingaddress2" placeholder="Line 2"/>
+      <label for="username">City:</label>
+      <input type="text" name="city" id="city" placeholder="City"/>
+      <label for="username">State:</label>
+      <input type="text" name="state" id="state" placeholder="State"/>
+      <label for="username">Post code:</label>
+      <input type="text" name="postcode" id="postcode" placeholder="Post code"/>
+      <label for="username">Country:</label>
+      <input type="text" name="country" id="country" placeholder="Country"/>
       <button class="g-recaptcha" data-sitekey="6Ler14gbAAAAADwj5Iq5vN-jm9TBTjZpFJqgSLpO" data-callback='onSubmitCreateAccount'>Create account</button>
     </form>
     <a href="${location.pathname}">Login instead &gt;</a>
@@ -24,8 +39,16 @@ export function renderCreateAccount() {
 window.onSubmitCreateAccount = token => {
   const form = document.querySelector('form');
   post('createAccount', {
+    name: form.name.value,
+    company: form.company.value,
     email: form.email.value,
     password: form.password.value,
+    billingaddress1: form.billingaddress1.value,
+    billingaddress2: form.billingaddress2.value,
+    city: form.city.value,
+    state: form.state.value,
+    postcode: form.postcode.value,
+    country: form.country.value,
     token
   }, (json, err) => {
     if (err || json.error) {
